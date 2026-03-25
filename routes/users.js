23cf7962a22284d0b1fb3010f4b311a4b81182e5
J.Boneo <joseann.marika04@gmail.com>
2026-03-24 20:18:28 -0400

    Created user form and sever logic for sending user

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res)=>{
+router.route('/').get((req, res)=>{
     res.send('User List');
+}).post((req,res) =>{
+    const firstName = req.body.firstName;
+    const isValid = firstName !=="";
+    if(isValid){
+        console.log(`Adding user: ${firstName}`);
+        users.push({firstName});
+        res.render('users/list',{users});
+    }
+    else{
+        console.log("Error adding a user!");
+        res.render("users/new",{firstName:firstName})
+    }
 });
+router.get('/list',(req,res) =>{
+    res.render('users/list',{users});
+})
 
 router.get('/new', (req, res)=>{
-    res.send('User New Form');
+    res.render('users/new',{firstName:"firstName"});
 });
 //dynamic route
 //router.get("/:id",(req,res) =>{
@@ -24,7 +39,7 @@ router.route('/:id').get((req, res)=>{
     res.send(`Updating User data for id: ${req.params.id}`);
 });
 
-const users =[{name:"George"}, {name:"Justyna"}];
+const users =[{firstName:"George"}, {firstName:"Justyna"}];
 router.param("id", (req, res, next, id)=>{
     req.user = users[id];
     console.log("Access attempted by User:",id);
